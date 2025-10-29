b243b666d2f34027ac38bd07ba16335df9b90127
Reinaldo-rs <reinaldosaopaulo1@hotmail.com>
2025-10-29 15:10:26 -0300

    feat(users): adiciona rotas de criação e listagem de usuários

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-import express, { response } from 'express'
+import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/users', (req, res) => {
+    
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
 
 app.get('/users', (req, res) => {
-    res.send('OK! Deu certo!')
+    res.status(200).json(users)
 })
 
 app.listen(3000)
